b128a321ca830004dcf0814b90f08cea10da401d
zsytssk <zsytssk@gmail.com>
2020-09-03 13:59:37 +0800

    master:

--- doc/note.md
+++ doc/note.md
@@ -1,5 +1,7 @@
 https://www.youtube.com/watch?v=FLQ-Vhw1NYQ&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ&index=4&t=982s
 
+https://dart.dev/tutorials
+
 ## 2020-09-02 09:58:34
 
 - @ques 如何在本地运行 dart 语言
--- src/test.dart
+++ src/test.dart
@@ -1,3 +1,20 @@
-void main() {
-  print("hello world");
+abstract class Animal {
+  String name;
+  Animal(this.name);
+}
+
+class Cat extends Animal {
+  String breed;
+
+  Cat(int i)
+      : breed = breedFromCode(i),
+        super(randomName());
+
+  static String breedFromCode(int i) {
+    // ...
+  }
+
+  static String randomName() {
+    // ...
+  }
 }
