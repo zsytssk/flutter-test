7d22586dd5b0bff2fa28f0bc50bf4c62f78920cc
zsytssk <zsytssk@gmail.com>
2020-09-09 10:14:04 +0800

    master:

--- doc/note.md
+++ doc/note.md
@@ -2,7 +2,24 @@ https://www.youtube.com/watch?v=FLQ-Vhw1NYQ&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6
 
 https://dart.dev/samples
 
+https://dart.dev/guides/language/language
+
+## 2020-09-09 10:03:03
+
+- dart tutorial for js
+  dart 高级的 js
+  with
+
 ## 2020-09-07 09:59:07
+
+- @todo (mixin)[https://dart.dev/guides/language/language-tour#adding-features-to-a-class-mixins]
+- @todo (abstract class)[https://dart.dev/guides/language/language-tour#abstract-classes]
+
+- @todo `async async*`
+
+- @ques @override 和普通调用 super.method 有什么区别
+
+- `DateTime.now().difference(launchDate).inDays ~/ 365;`
 
 - @ques 如何写 `import 'path/to/my_other_file.dart';`
   - 本地 package
--- src/test.dart
+++ src/test.dart
@@ -1,5 +1,49 @@
-import 'dart:math';
+class Spacecraft {
+  String name;
+  DateTime launchDate;
+  Spacecraft(this.name, this.launchDate) {}
+  Spacecraft.unlaunched(String name) : this(name, null);
+  int get launchYear => this.launchDate.year;
+  void describe() {
+    print('Spacecraft $name');
+    if (launchDate != null) {
+      int year = DateTime.now().difference(launchDate).inDays ~/ 365;
+      print('Launched: $launchYear ($year year ago)');
+    } else {
+      print('unluanched');
+    }
+  }
+}
+
+class Piloted {
+  int astronauts = 1;
+  void describeCrew() {
+    print('Number of astronauts: $astronauts');
+  }
+}
+
+class Piloted2 {
+  int astronauts2 = 2;
+  void describeCrew() {
+    print('Number of astronauts2: $astronauts2');
+  }
+}
+
+class Orbiter extends Spacecraft with Piloted, Piloted2 {
+  double altitude;
+  Orbiter(String name, DateTime launchDate, double altitude)
+      : this.altitude = altitude,
+        super(name, launchDate) {
+    print('Orbiter');
+  }
+  void describe() {
+    print('Orbiter altitude: $altitude');
+    super.describeCrew();
+    super.describe();
+  }
+}
 
 void main() {
-  print(pi);
+  var voyager = Orbiter('Voyager I', DateTime(1977, 9, 5), 12.4);
+  voyager.describe();
 }
